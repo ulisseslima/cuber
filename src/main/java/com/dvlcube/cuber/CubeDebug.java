d227bdc98b083cc8df0f4515093a5e1474248752
dvlcube <ulisses@dvlcube.com>
2013-07-07 04:30:42 -0300

    cube debug

---
+++ src/main/java/com/dvlcube/cuber/CubeDebug.java
@@ -0,0 +1,16 @@
+package com.dvlcube.cuber;
+
+/**
+ * 
+ * @since 06/07/2013
+ * @author wonka
+ */
+public class CubeDebug {
+	public void sleep(int millis) {
+		DebugUtils.sleep(millis);
+	}
+
+	public void sleeps(int seconds) {
+		DebugUtils.sleeps(seconds);
+	}
+}
