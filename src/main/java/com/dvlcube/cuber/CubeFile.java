edc3c4f9a920a7db7253b17df20337640f6acff6
dvlcube <ulisses@dvlcube.com>
2013-07-07 22:56:25 -0300

    head(n), firstLine()

--- src/main/java/com/dvlcube/cuber/CubeFile.java
+++ src/main/java/com/dvlcube/cuber/CubeFile.java
@@ -1,7 +1,10 @@
 package com.dvlcube.cuber;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 /**
  * 
@@ -45,5 +48,49 @@ public class CubeFile {
 
 	public boolean isDir() {
 		return o.isDirectory();
+	}
+
+	public String firstLine() {
+		return FileUtils.firstLine(o);
+	}
+
+	public List<String> head(int lines) {
+		return FileUtils.head(o, lines);
+	}
+
+	/**
+	 * Appends a line to the file, line ends with line break.
+	 * 
+	 * @param line
+	 *            line to append.
+	 * @return this.
+	 * @since 07/07/2013
+	 * @author wonka
+	 */
+	public CubeFile append(String line) {
+		try (PrintWriter writer = new PrintWriter(o)) {
+			writer.println(line);
+			writer.flush();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return this;
+	}
+
+	/**
+	 * @param extension
+	 *            the desired extension.
+	 * @return true if the file has the passed extension.
+	 * @since 07/07/2013
+	 * @author wonka
+	 */
+	public boolean is(String extension) {
+		if (o == null)
+			throw new IllegalStateException("file was not initialized");
+
+		if (o.getPath().endsWith(extension))
+			return true;
+
+		return false;
 	}
 }
