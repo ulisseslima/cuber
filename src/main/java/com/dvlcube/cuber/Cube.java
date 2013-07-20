db6fa18d29858950e1e307dc2aec078c97083a7c
unknown <wonka@.(none)>
2013-07-20 14:52:30 -0300

    in(...), join(separator)

--- src/main/java/com/dvlcube/cuber/Cube.java
+++ src/main/java/com/dvlcube/cuber/Cube.java
@@ -9,15 +9,53 @@ import com.dvlcube.cuber.utils.StringUtils;
  * @since 14/04/2013
  */
 public class Cube {
+	public String[] arr;
 	public Object o;
 
 	/**
 	 * @param object2
 	 * @author wonka
+	 * @param objects
 	 * @since 14/04/2013
 	 */
-	public Cube(Object object) {
+	public Cube(Object object, String... objects) {
 		o = object;
+		arr = objects;
+	}
+
+	/**
+	 * @param objects
+	 *            objects to compare.
+	 * @return true if this object is equal to any of the other objects.
+	 * @since 07/07/2013
+	 * @author wonka
+	 */
+	public boolean in(Object... objects) {
+		return ObjectUtils.in(o, objects);
+	}
+
+	/**
+	 * @return a string representing the junction between the objects, separated by commas.
+	 * @author wonka
+	 * @since 20/07/2013
+	 */
+	public String join() {
+		return join(",");
+	}
+
+	/**
+	 * @param separator
+	 * @return a string representing the junction between the objects, using the provided separator.
+	 * @author wonka
+	 * @since 20/07/2013
+	 */
+	public String join(String separator) {
+		StringBuilder builder = new StringBuilder(separator);
+		builder.append(o.toString());
+		for (String string : arr) {
+			builder.append(separator).append(string);
+		}
+		return builder.toString().replaceFirst(separator, "");
 	}
 
 	/**
@@ -43,17 +81,6 @@ public class Cube {
 	 */
 	public String stringify() {
 		return StringUtils.stringify(o);
-	}
-
-	/**
-	 * @param objects
-	 *            objects to compare.
-	 * @return true if this object is equal to any of the other objects.
-	 * @since 07/07/2013
-	 * @author wonka
-	 */
-	public boolean in(Object... objects) {
-		return ObjectUtils.in(o, objects);
 	}
 
 	@Override
